c840e9be9c3e4c2d86cc77422d4d10aaab4f1822
sm <sm@mail.com>
2023-02-01 12:39:11 +0100

    .env init

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -3,24 +3,24 @@ import { useLoadScript, GoogleMap } from "@react-google-maps/api"
 
 export default function Map() {
     const [myPosition, setMyposition] = useState({});
-const {isLoaded} = useLoadScript({
-    googleMapsApiKey: "AIzaSyD_wEyaEMHjI6AK-gTEBhxoUYbGS6aD2kg",
-})
-      useEffect(() => {
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+    })
+    useEffect(() => {
         navigator.geolocation.getCurrentPosition((pos) => {
             setMyposition(pos.coords)
         })
-      }
-     , [])
+    }
+        , [])
 
-        return <div>
-            {!isLoaded && <p>Loading...</p>}
-            {isLoaded && 
-            <GoogleMap 
-            zoom={10} 
-            center={myPosition ? {lat: myPosition.latitude, lng: myPosition.longitude} 
-            : {lat: 0, lng: 0}} 
-            mapContainerClassName="mapContainer"></GoogleMap>}
-        </div>
+    return <div>
+        {!isLoaded && <p>Loading...</p>}
+        {isLoaded &&
+            <GoogleMap
+                zoom={10}
+                center={myPosition ? { lat: myPosition.latitude, lng: myPosition.longitude }
+                    : { lat: 0, lng: 0 }}
+                mapContainerClassName="mapContainer"></GoogleMap>}
+    </div>
 
 }
