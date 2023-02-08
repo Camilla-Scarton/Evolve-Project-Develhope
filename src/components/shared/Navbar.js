82ccc0900cf65fa562cd9701aa26fd54726ffabd
Camilla Scarton <camy.s96@gmail.com>
2023-02-09 00:39:41 +0100

    navbar with links

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,15 +1,17 @@
-import logo from "../../assets/logo.png";
 import logo3 from "../../assets/logo3.png";
 import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <div className="z-10 flex sticky top-0 h-[60px] text-white bg-blue-900 dark:bg-purple-800">
-      <Link to="/map">Map</Link>
-      <img className="w-[60px] h-[60px]" src={logo} alt="LOGO" />
-      <div className="flex w-full items-center justify-center">
-        <img className="h-[90px]" src={logo3} alt="logo"/>
-      </div>
-    </div>
+    <navbar className="gap-5 z-10 flex items-center text-center sticky top-0 h-[60px] text-white bg-blue-900 dark:bg-purple-800">
+      <img className="px-4 h-[90px]" src={logo3} alt="logo"/>
+      <Link to="/">Home</Link>
+      <>|</>
+      <Link to="/map">Nearest gyms</Link>
+      <>|</>
+      <Link to="/bmi">Daily calories</Link>
+      <>|</>
+      <Link to="/design">Design</Link>
+    </navbar>
   );
 }
