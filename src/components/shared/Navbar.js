11c08eb10f6f9eba616b1dac05f8a5dd078b00ed
Camilla Scarton <camy.s96@gmail.com>
2023-02-20 17:39:27 +0100

    spaced navbar

--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -3,15 +3,17 @@ import { Link } from "react-router-dom";
 
 export function Navbar() {
   return (
-    <nav className="gap-5 z-10 flex items-center text-center sticky top-0 h-20 text-white bg-blue-900 dark:bg-purple-800">
+    <nav className="flex items-center text-center sticky top-0 h-20 text-white bg-blue-900 dark:bg-purple-800 place-content-around">
       <img className="px-4 h-[90px]" src={logo3} alt="logo"/>
-      <Link to="/">Home</Link>
-      <>|</>
-      <Link to="/map">Nearest gyms</Link>
-      <>|</>
-      <Link to="/bmi">Daily calories</Link>
-      <>|</>
-      <Link to="/design">Design</Link>
+      <div className="flex gap-10 items-center">
+        <Link to="/">Home</Link>
+        <>|</>
+        <Link to="/map">Nearest gyms</Link>
+        <>|</>
+        <Link to="/bmi">Daily calories</Link>
+        <>|</>
+        <Link to="/design">Design</Link>
+      </div>
     </nav>
   );
 }
